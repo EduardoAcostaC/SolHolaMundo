97c9c1e2a4f11e2a1a2dab78adf51974f74b1c3c
EduardoAcostaC <eacostac1700@alumno.ipn.mx>
2023-08-02 21:49:43 -0600

    Agregar archivos de proyecto.

---
+++ HolaMundo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HolaMundo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Inicio()
+        {
+            return View("VistaUno");
+        }
+
+        public ActionResult VistaDos()
+        {
+            return View("VistaDos");
+        }
+
+        public ActionResult VistaTres()
+        {
+            return View("VistaTres");
+        }
+    }
+}
